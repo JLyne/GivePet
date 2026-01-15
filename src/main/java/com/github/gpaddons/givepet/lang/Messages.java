4a42d7f86b055b1e290d4a0548ea2568f62acc5a
James Lyne <jim+github@not-null.co.uk>
2026-01-15 20:31:46 +0000

    Message fixes

--- src/main/java/com/github/gpaddons/givepet/lang/Messages.java
+++ src/main/java/com/github/gpaddons/givepet/lang/Messages.java
@@ -5,20 +5,20 @@ import org.jetbrains.annotations.NotNull;
 
 public enum Messages implements ConfigMessage {
 
-  SEND_PENDING_FROM("send.pending.from", "<red>You already have a pending transfer to <aqua><recipient>!<red> Please wait for them to respond."),
-  SEND_PENDING_TO("send.pending.to", "<aqua><recipient><green> already has a pending pet transfer! Please wait for them to respond."),
-  SEND_TARGET_PET("send.target_pet", "<red>You must target one of your pets to transfer!"),
-  SEND_NO_RECIPIENT("send.no_recipient", "<red>Invalid recipient!"),
+  SEND_PENDING_FROM("send.pending.from", "<red>You already have a pending transfer to <aqua><recipient>.<red> Please wait for them to respond."),
+  SEND_PENDING_TO("send.pending.to", "<aqua><recipient><red> already has a pending pet transfer. Please wait for them to respond."),
+  SEND_TARGET_PET("send.target_pet", "<red>You must target one of your pets to transfer."),
+  SEND_NO_RECIPIENT("send.no_recipient", "<red>Invalid recipient."),
   SEND_OFFER("send.offer", "<aqua><owner><green> would like to transfer <aqua>[<pet>]<green> to you!\nYou have two minutes to <aqua>[<acceptpet>]<green> or <aqua>[<declinepet>]<green>."),
-  SEND_OFFERED("send.offered", "<green>Offered <aqua>[<pet>]<green> to <aqua><recipient><green>! They have two minutes to respond."),
+  SEND_OFFERED("send.offered", "<green>Offered <aqua>[<pet>]<green> to <aqua><recipient>.<green> They have two minutes to respond."),
 
-  RECEIVE_NO_PENDING("receive.no_pending", "<red>You don't have any pending pet transfers!"),
-  RECEIVE_NOT_FOUND_SENDER("receive.not_found.sender", "<red>Unable to transfer pet! Please make sure you stay near it until the transfer completes."),
-  RECEIVE_NOT_FOUND_RECIPIENT("receive.not_found.recipient", "<red>Unable to locate pet!"),
-  RECEIVE_ACCEPT_SENDER("receive.accept.sender", "<green>Pet transferred!"),
-  RECEIVE_ACCEPT_RECIPIENT("receive.accept.recipient", "<green>Pet transferred!"),
-  RECEIVE_DECLINE_SENDER("receive.decline.sender", "<red>Pet transfer declined!"),
-  RECEIVE_DECLINE_RECIPIENT("receive.decline.recipient", "<green>Declined pet transfer from <aqua><owner><green>!");
+  RECEIVE_NO_PENDING("receive.no_pending", "<red>You don't have any pending pet transfers."),
+  RECEIVE_NOT_FOUND_SENDER("receive.not_found.sender", "<red>Unable to transfer pet. Please make sure you stay near it until the transfer completes."),
+  RECEIVE_NOT_FOUND_RECIPIENT("receive.not_found.recipient", "<red>Unable to locate pet."),
+  RECEIVE_ACCEPT_SENDER("receive.accept.sender", "<green>Pet transferred."),
+  RECEIVE_ACCEPT_RECIPIENT("receive.accept.recipient", "<green>Pet transferred."),
+  RECEIVE_DECLINE_SENDER("receive.decline.sender", "<red>Pet transfer declined."),
+  RECEIVE_DECLINE_RECIPIENT("receive.decline.recipient", "<green>Declined pet transfer from <aqua><owner>."),
 
   private final String key;
   private final String defaultValue;
